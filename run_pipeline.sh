97d45aea4ce34c7c351bee81b12a6f12a04e7661
database-tycoon <stephen@databasetycoon.com>
2024-11-12 11:59:15 -0500

    Shell script updates

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Run ingestion
-python3 src/pipeline/ingest/run.py
-wait
+echo "Starting pipeline"
 
+# Run ingestion
+echo "Start ingestion"
+.venv/bin/python3 src/pipeline/ingest/run.py
+wait
+echo "End ingestion"
+
+
+echo "Start sqlMesh"
 # Run sqlmesh
 cd sqlMesh
 sqlmesh run
 wait
+echo "End sqlMesh"
 
 # Run upload
 cd ..
-python3 src/pipeline/upload/run.py
+.venv/bin/python3 src/pipeline/upload/run.py
 wait
